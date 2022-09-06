0ec2f389d0372c005766e16aacf2c2601af15777
Davidgraja <juliograjalesparedes127@gmail.com>
2022-09-06 22:18:48 +0200

    se organizo el codigo para una mejor lectura y escabilidad

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,39 +1,26 @@
-// eventos  sobre el input email login
-// const emailInput = document.getElementById("email")
-let iconEmail = document.querySelector(".bi-envelope")
-let emailInput= document.querySelector(".email")
+// evento  sobre el input email login con callback 
+let emailInput =document.querySelector(".email")
 
+emailInput.addEventListener('keyup',()=>{
+    let evento = "./events.js"
+    import(evento)
 
-
-
-
-
-emailInput.addEventListener("keyup",()=>{
-    let pattern =/^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
-    if(emailInput.value===""){
-        return  iconEmail.style.color= "rgba(0,0,0,0.6)" 
-    }
-    if(emailInput.value.match(pattern)){
-        iconEmail.classList.replace("bi-envelope" , "bi-check2-circle")
-        return iconEmail.style.color="rgba(0,255,0,0.8)"
-    }    
-
-    iconEmail.classList.replace("bi-check2-circle" , "bi-envelope")
-    iconEmail.style.color="rgba(255,0,0,0.4)"
+    .then(evento=>{
+        evento.eventEmail()
+    })
+    .catch(error=> console.log(error))
 })
 
 
+// evento en el  icono del input password   con asincronismo 
 
-
-// evento en el  icono del input password  en la  vista login
-
-let iconPassword ;
+let iconPassword
 
 Promise.resolve( iconPassword = document.querySelector(".bi-eye"))
 .then(icon =>{
     icon.addEventListener("click",async function(){
         let evento  = await import("./events.js")
-        evento.eventPasswordLogin()
+        evento.eventPassword()
     })
 })
 .catch(error => console.log(error))
