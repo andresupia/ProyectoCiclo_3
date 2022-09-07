191f204bab96ea4febe715491fa571eb727a807d
Davidgraja <juliograjalesparedes127@gmail.com>
2022-09-07 19:42:20 +0200

    se añadio nuevos eventos , en la opción añadir libro , estoy buscando la forma de hacer el codigo javaScript mas escalable

--- javaScript/events.js
+++ javaScript/events.js
@@ -1,6 +1,25 @@
+// evento sobre el icono del input email
+export function eventEmail(){
 
-// evento  sobre el icono del  input password
-export function eventPasswordLogin(){
+    let iconEmail = document.querySelector(".bi-envelope")
+    emailInput.addEventListener("keyup",()=>{
+        let pattern =/^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
+        if(emailInput.value===""){
+            return  iconEmail.style.color= "rgba(0,0,0,0.6)" 
+        }
+        if(emailInput.value.match(pattern)){
+            iconEmail.classList.replace("bi-envelope" , "bi-check2-circle")
+            return iconEmail.style.color="rgba(0,255,0,0.8)"
+        }    
+    
+        iconEmail.classList.replace("bi-check2-circle" , "bi-envelope")
+        iconEmail.style.color="rgba(255,0,0,0.4)"
+    })
+}
+
+
+// evento sobre el icono del input password
+export function eventPassword(){
     const inputPassword = document.getElementById("password")
     
     iconPassword.classList.replace("bi-eye-slash" , "bi-eye")
@@ -11,5 +30,64 @@ export function eventPasswordLogin(){
     }else{
         inputPassword.type= "password"
 
+    }
+}
+
+
+
+// evento cancelar
+ export function cancelar(cancel){
+    
+    let bottonCancelar = document.querySelector(".botton-cancelar")
+    bottonCancelar.addEventListener("click",()=>{
+        cancel.classList.remove("open-container-añadir")
+    })
+}
+
+
+// clase para la creacion de  libros 
+
+class libro{
+    #nombre;
+    #autor
+    #editorial
+    #cantidad
+    constructor(nombre , autor, editorial , cantidad){
+        this.#nombre = nombre
+        this.#autor = autor
+        this.#editorial = editorial
+        this.#cantidad = cantidad
+    }
+
+    get nombre(){
+        return this.#nombre
+    }
+
+    set nombre(newNombre){
+        this.#nombre = newNombre
+    }
+
+    get autor(){
+        return this.#autor
+    }
+
+    set autor(newAutor){
+        this.#autor = newAutor
+    }
+
+    get editorial(){
+        return this.#editorial
+    }
+
+    set editorial(newEditorial){
+        this.#editorial = newEditorial
+    }
+
+    get cantidad(){
+        return this.#cantidad
+    }
+
+    set cantidad(newCantidad){
+        this.#cantidad = newCantidad
     }
 }
--- javaScript/main.js
+++ javaScript/main.js
@@ -1,38 +1,77 @@
-// eventos  sobre el input email login
-// const emailInput = document.getElementById("email")
-let iconEmail = document.querySelector(".bi-envelope")
-let emailInput= document.querySelector(".email")
 
-
-emailInput.addEventListener("keyup",()=>{
-    let pattern =/^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
-    if(emailInput.value===""){
-        return  iconEmail.style.color= "rgba(0,0,0,0.6)" 
-    }
-    if(emailInput.value.match(pattern)){
-        iconEmail.classList.replace("bi-envelope" , "bi-check2-circle")
-        return iconEmail.style.color="rgba(0,255,0,0.8)"
-    }    
-
-    iconEmail.classList.replace("bi-check2-circle" , "bi-envelope")
-    iconEmail.style.color="rgba(255,0,0,0.4)"
-})
-
-
-
-
-// evento en el  icono del input password  en la  vista login
-let iconPassword ;
-
-Promise.resolve( iconPassword = document.querySelector(".bi-eye"))
-.then(icon =>{
-    icon.addEventListener("click",async function(){
-        let evento  = await import("./events.js")
-        evento.eventPasswordLogin()
+// Evento sobre el icono del input email
+let emailInput ;
+Promise.resolve(emailInput =document.querySelector(".email"))
+.then(()=>{
+    emailInput.addEventListener("keyup",async function(){
+        let modulo = await import("./events.js")
+        modulo.eventEmail()
     })
 })
 .catch(error => console.log(error))
 
 
 
+// Evento sobre el icono del input del password
 
+let iconPassword
+
+Promise.resolve( iconPassword = document.querySelector(".bi-eye"))
+.then(icon =>{
+    icon.addEventListener("click",async function(){
+        let modulo  = await import("./events.js")
+        modulo.eventPassword()
+    })
+})
+.catch(error => console.log(error))
+
+
+// evento añadir libro
+
+
+
+let añadirLibro;
+Promise.resolve( añadirLibro = document.querySelector(".botton-añadir-libro"))
+.then(()=>{
+    añadirLibro.addEventListener("click",()=>{
+        let containerAñadirlibro = document.querySelector(".container-añadir-libro") 
+        containerAñadirlibro.classList.toggle("open-container-añadir")
+
+
+        let modulo="./events.js"
+        import(modulo)
+        .then(modulo =>{
+            modulo.cancelar(containerAñadirlibro)
+        })
+        .catch(error => console.log(error))
+      
+        
+    })
+})
+
+
+
+
+// atrapando la informacion ingresada por el usuario en la opcion añadir libro
+
+let form_añadir = document.getElementById("form-añadir-libro")
+
+form_añadir.onsubmit=(e)=>{
+        e.preventDefault()
+
+        let nombre_libro = document.getElementById("nombre-libro")
+        let autor_libro = document.getElementById("autor-libro")
+        let editorial_libro = document.getElementById("editorial-libro")
+        let cantidad_libro = document.getElementById("cantidad-libro")
+
+        let valor_nombre = nombre_libro.value
+        let valor_autor_libro = autor_libro.value
+        let valor_editorial = editorial_libro.value
+        let valor_cantidad = cantidad_libro.value
+        
+        console.log(`nombre: ${valor_nombre} , autor: ${valor_autor_libro}  editorial: ${valor_editorial} cantidad: ${valor_cantidad}`)
+        nombre_libro.value = ""
+        autor_libro.value= ""
+        editorial_libro.value = ""
+        cantidad_libro.value = ""
+}     
