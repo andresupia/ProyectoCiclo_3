8fd9aedbcc1936455777712c03bfa084945fd2cb
Davidgraja <juliograjalesparedes127@gmail.com>
2022-09-08 20:33:37 +0200

    se elimino el archivo event.js  ya que no era necesario , ademas se cambio el archivo de main.js a login_registro.js

--- javaScript/login_registro.js
+++ javaScript/login_registro.js
@@ -1,27 +1,39 @@
 // evento  sobre el input email login con callback 
 let emailInput =document.querySelector(".email")
+let iconEmail = document.querySelector(".bi-envelope")
 
 emailInput.addEventListener('keyup',()=>{
-    let evento = "./events.js"
-    import(evento)
 
-    .then(evento=>{
-        evento.eventEmail()
-    })
-    .catch(error=> console.log(error))
+    let pattern =/^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
+
+    if(emailInput.value===""){
+         iconEmail.classList.replace("bi-check2-circle" , "bi-envelope")
+        return  iconEmail.style.color= "rgba(0,0,0,0.6)" 
+    }
+    if(emailInput.value.match(pattern)){
+        iconEmail.classList.replace("bi-envelope" , "bi-check2-circle")
+        return iconEmail.style.color="rgba(0,255,0,0.8)"
+    }    
+
+    iconEmail.classList.replace("bi-check2-circle" , "bi-envelope")
+    iconEmail.style.color="rgba(255,0,0,0.4)"
 })
 
 
 // evento en el  icono del input password   con asincronismo 
 
-let iconPassword
+let iconPassword= document.querySelector(".bi-eye")
 
-Promise.resolve( iconPassword = document.querySelector(".bi-eye"))
-.then(icon =>{
-    icon.addEventListener("click",async function(){
-        let evento  = await import("./events.js")
-        evento.eventPassword()
-    })
+iconPassword.addEventListener("click", ()=>{
+    let inputPassword = document.getElementById("password")
+    
+    iconPassword.classList.replace("bi-eye-slash" , "bi-eye")
+
+    if(inputPassword.type == "password"){
+        iconPassword.classList.replace("bi-eye" , "bi-eye-slash")
+        inputPassword.type= "text"
+    }else{
+        inputPassword.type= "password"
+
+    }
 })
-.catch(error => console.log(error))
-
