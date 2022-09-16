808628e2ba64543bc16b832ae204539c349c4e4a
Davidgraja <juliograjalesparedes127@gmail.com>
2022-09-16 17:00:22 +0200

    se modifico los eventos de eliminar , editar y buscar mejorando asi el funcionamiento de estos y eliminando los errores que estaban presentando

--- javaScript/main.js
+++ javaScript/main.js
@@ -87,15 +87,91 @@ añadirLibro.addEventListener("click",()=>{
 
 
 
-// atrapando la informacion ingresada por el usuario en la opcion añadir libro
-
-let form_añadir = document.getElementById("form-añadir-libro")
 
 // array con los objetos libros
 const lista_libros = []
 
-// contenedor padre
+// funcion para remover libros dentro del array y el DOM
+function eliminar(){
+let eventoEliminar = document.querySelectorAll(".boton-eliminar")
+eventoEliminar.forEach((elemento)=>{
+    elemento.addEventListener("click",()=>{
+
+      
+        elemento.parentElement.parentElement.remove()
+        
+        encontrado =  lista_libros.find(e=> e.nombre ==  elemento.parentElement.parentElement.children[0].textContent)
+        let indice = lista_libros.findIndex(element=> element==encontrado)
+        lista_libros.splice(indice,1)
+        
+        })
+    })
+
+}
+
+
+// funcion para editar un libro en el Dom y dentro de array
+function editar(){
+    let encontrado;
+    let eventEditar= document.querySelectorAll(".bottom-editar")
+    let arrayEditar = Array.from(eventEditar)
+    arrayEditar.forEach((e,i,a)=>{
+        e.addEventListener('click',()=>{
+
+            let contenedor_editar = document.getElementById("container-editar-libro")
+            contenedor_editar.classList.toggle("open-container-editar")
+            
+            cancelar(contenedor_editar , "open-container-editar")
+                    
+            encontrado =  lista_libros.find(element=> element.nombre ==  e.parentElement.parentElement.children[0].textContent)
+            console.log(lista_libros.includes(encontrado))
+            let indice = lista_libros.findIndex(element=> element==encontrado)
+               
+            let form_editar = document.getElementById("form-editar-libro")
+            form_editar.onsubmit=(evento)=>{
+                evento.preventDefault()
+
+                let nuevo_nombre = document.getElementById("nuevo-nombre-libro")
+                let nuevo_autorlibro = document.getElementById("nuevo-autor-libro")
+                let nuevo_editorial_libro = document.getElementById("nuevo-editorial-libro")
+                let nuevo_cantidad_libro = document.getElementById("nuevo-cantidad-libro")
+                let nuevo_categoria_libro = document.getElementById("nuevo-categoria-libro")
+        
+                  
+                    e.parentElement.parentElement.children[0].textContent  = nuevo_nombre.value
+
+                    e.parentElement.children[0].children[1].textContent  = nuevo_autorlibro.value
+                    e.parentElement.children[1].children[1].textContent  = nuevo_editorial_libro.value
+                    e.parentElement.children[2].children[1].textContent  = nuevo_categoria_libro.value
+                    e.parentElement.children[3].children[1].textContent  = nuevo_cantidad_libro.value
+
+                    lista_libros[indice].nombre = nuevo_nombre.value
+                    lista_libros[indice].autor = nuevo_autorlibro.value
+                    lista_libros[indice].editorial = nuevo_editorial_libro.value
+                    lista_libros[indice].categoria = nuevo_categoria_libro.value
+                    lista_libros[indice].cantidad = nuevo_cantidad_libro.value
+
+                    nuevo_nombre.value = ""
+                    nuevo_autorlibro. value= ""
+                    nuevo_editorial_libro.value = ""
+                    nuevo_categoria_libro.value = ""
+                    nuevo_cantidad_libro.value = ""
+    
+    
+                    contenedor_editar.classList.remove("open-container-editar")
+
+            }           
+        })
+    })
+}
+
+
+
+
+// atrapando y mostrando la informacion ingresada por el usuario en la opcion añadir libro
 let contenedor_libros  = document.getElementById("container-libros")
+
+let form_añadir = document.getElementById("form-añadir-libro")
 
 form_añadir.onsubmit=(e)=>{
         e.preventDefault()
@@ -112,112 +188,39 @@ form_añadir.onsubmit=(e)=>{
         let valor_editorial = editorial_libro.value
         let valor_cantidad = cantidad_libro.value
         let valor_categoria = categoria_libro.value
-
-        
+  
         let newLibro = new libro(valor_nombre , valor_autor_libro , valor_editorial , valor_categoria , valor_cantidad)
         lista_libros.push(newLibro)
-        
-        
+
+
+        let containerAñadirlibro = document.querySelector(".container-añadir-libro") 
+        containerAñadirlibro.classList.remove("open-container-añadir")
     
+        contenedor_libros.innerHTML+=` <details class="card-libro">
+        <summary>${newLibro.nombre}</summary>
+        <div>
+        <p> <span class="text-muted">autor : </span> <span>${newLibro.autor}</span> </p>
+        <p><span class="text-muted">Editorial :</span> <span>${newLibro.editorial}</span></p>
+        <p><span class="text-muted">Categoria :</span> <span>${newLibro.categoria}</span> </p>
+        <p><span class="text-muted">Cantidad :</span>  <span> ${newLibro.cantidad}</span></p>
+        <button class="btn bg-info text-white bottom-editar">Editar</button>
+        <button class=" btn bg-danger text-white float-end boton-eliminar">Eliminar</button>
+        </div>
+    </details>`
+
         
-         
-
-        // etiqueta contenedora de la informacion capturada 
-        let container_card = document.createElement("article")
-        container_card.classList.add('article-card')
-        
-        container_card.innerHTML=`<div class="card-body">
-
-                                <h4 class="card-title mb-3">${newLibro.nombre}</h4>
-                                <h5 class="card-subtitle text-muted border-bottom mb-3 pb-2 ">${newLibro
-                                .autor}</h5>
-                                <p class="card-text"> <span class="text-muted">Editorial : </span> <span class="text-dark">${newLibro.editorial}</span> </p>
-                                <p class="card-text"> <span class="text-muted">Categoria : </span> <span class="text-dark">${newLibro.categoria}</span> </p>
-                                <p class="card-text"> <span class="text-muted">Cantidad : </span> <span class="text-dark">${newLibro.cantidad}</span> </p>
-
-                                <button class="btn text-white bottom-editar bg-primary ">Editar</button>
-                                <button class="btn text-white float-end bg-danger bottom-eliminar">Eliminar</button>
-                               
-                                </div>`
-
-        contenedor_libros.prepend(container_card)
-   
         nombre_libro.value = ""
         autor_libro.value= ""
         editorial_libro.value = ""
         cantidad_libro.value = ""
         categoria_libro.value=""
-        
-        let containerAñadirlibro = document.querySelector(".container-añadir-libro") 
-        containerAñadirlibro.classList.remove("open-container-añadir")
 
-        
-        // evento eliminar 
-        let eventEliminar = document.querySelector('.bottom-eliminar')
-        eventEliminar.addEventListener("click",()=> {
-
-            try {
-
-                let confirmacion= prompt('¿ estas seguro de eliminar este libro ? \n si \n no').toLowerCase()
-                
-                if(confirmacion == "si"){
-                    eventEliminar.parentElement.parentElement.remove()
-                }
-
-            } catch (error) {
-                console.log("confirmacion no ingresada")
-            }
-           
-
-        })
-
-
-        // evento editar
-
-        let eventEditar= document.querySelector(".bottom-editar")
-        eventEditar.addEventListener("click", ()=>{
-            let contenedor_editar = document.getElementById("container-editar-libro")
-            contenedor_editar.classList.toggle("open-container-editar")
     
-            cancelar(contenedor_editar , "open-container-editar")
-    
-            let nuevo_nombre = document.getElementById("nuevo-nombre-libro")
-            let nuevo_autorlibro = document.getElementById("nuevo-autor-libro")
-            let nuevo_editorial_libro = document.getElementById("nuevo-editorial-libro")
-            let nuevo_cantidad_libro = document.getElementById("nuevo-cantidad-libro")
-            let nuevo_categoria_libro = document.getElementById("nuevo-categoria-libro")
-    
+        eliminar()
+        editar()
+}   
 
-            let form_editar = document.getElementById("form-editar-libro")
-            form_editar.onsubmit=(e)=>{
-                e.preventDefault()
 
-                    container_card.children[0].children[0].textContent = nuevo_nombre.value
-                    container_card.children[0].children[1].textContent = nuevo_autorlibro.value
-                    container_card.children[0].children[2].children[1].textContent = nuevo_editorial_libro.value
-                    container_card.children[0].children[3].children[1].textContent = nuevo_categoria_libro.value
-                    container_card.children[0].children[4].children[1].textContent = nuevo_cantidad_libro.value
-    
-                    newLibro.nombre = nuevo_nombre.value
-                    newLibro.autor = nuevo_autorlibro.value
-                    newLibro.editorial = nuevo_editorial_libro.value
-                    newLibro.categoria = nuevo_categoria_libro.value
-                    newLibro.cantidad = nuevo_cantidad_libro.value
-                        
-                    nuevo_nombre.value = ""
-                    nuevo_autorlibro. value= ""
-                    nuevo_editorial_libro.value = ""
-                    nuevo_categoria_libro.value = ""
-                    nuevo_cantidad_libro.value = ""
-    
-    
-                    contenedor_editar.classList.remove("open-container-editar")
-
-            }
-
-        })
-
-}
 
 // evento en el buscador 
 let buscador = document.getElementById("buscador")
@@ -231,7 +234,10 @@ buscador.addEventListener("keyup" , (e) =>{
     for (let index = 0; index < contenedor_libros.children.length; index++) {
         let  valor = contenedor_libros.children[index]
 
-        if(er.test(valor.children[0].children[0].textContent) || er.test(valor.children[0].children[1].textContent) ){
+        // console.log(valor.children[0].textContent)
+        // console.log(valor.children[1].children[0].children[1].textContent)
+
+        if(er.test(valor.children[0].textContent) || er.test(valor.children[1].children[0].children[1].textContent) ){
 
             valor.classList.remove("filter")
 
