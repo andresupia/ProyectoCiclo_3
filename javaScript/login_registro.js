ddfb5c7f48ef55051482e9f9dc0240c976d7dcb5
Davidgraja <juliograjalesparedes127@gmail.com>
2022-09-26 00:59:08 +0200

    se realizo las conexiones a la api para el registro , login y diversas validaciones en base a la informacion dentro que nos entrega la api

--- javaScript/login_registro.js
+++ javaScript/login_registro.js
@@ -62,68 +62,84 @@ function alertas(claseDiv , textoP){
 
 
 
+
 // peticion de creacion de usuario  a la api 
 
 const API = 'http://localhost:8080/usuario'
 
-async function obtenerUsuario(){
-    let users = await fetch(API)
+async function obtenerUsuario(urlApi){
+    let users = await fetch(urlApi)
     let data  = await users.json()
     return data
 }
 
 
 async function crearUnUsuario(alias , nombres , apellidos, cedula , direccion, telefono , correo, contraseña){
-        let usuarios = obtenerUsuario()
-        usuarios.then(async usrs=> {
-        let arrayUsuario = Array.from(usrs)
-        let usuario_encontrado = arrayUsuario.find(e=>  e.idcedula_usr ==cedula  )
 
+        let usuarios = obtenerUsuario(API)
+        usuarios.then(async usrs=> {
+            let arrayUsuario = Array.from(usrs)
+            let cedula_encontrado = arrayUsuario.find(e=>  e.idcedula_usr ==cedula)
+            let correo_encontrado = arrayUsuario.find(e=> e.correo_usr == correo)
+            let contraseña_encontrada = arrayUsuario.find(e=> e.contrasena_usr == contraseña)
+                
             
-        
-        if(usuario_encontrado){
-        
-            alertas('alert-denied','Lo sentimos la cédula que ha ingresado ya existe, Intentelo de nuevo')
-        
-        }else{
+            if(cedula_encontrado){
+                
+                alertas('alert-denied','Lo sentimos la cédula ingresada ya existe, Esto se debe a que ya tiene una cuenta registrada bajo esta cédula , Por favor verifique si esta ingresando correctamente su  cedula o intente hacer Login')
+                
+            }
+            else if(correo_encontrado){
+                alertas('alert-denied','Lo sentimos el correo ingresado ya existe, Esto se debe a que ya tiene una cuenta registrada bajo este correo, Por favor verifique si esta ingresando correctamente su correo  o intente hacer Login' )
+            }
+            else if(contraseña_encontrada){
+                alertas('alert-denied','Lo sentimos la contraseña ingresada ya se encuentra en uso , Intente con otra contraseña')
+            }
+            else{
             
-            let crearUsuario = await fetch(API,{
-             method:'POST',
-             mode:'cors',
-             credentials:'same-origin',
-             headers:{
-                 'Content-Type' : 'application/json;'
-             },
-             body:JSON.stringify({
-                 'idcedula_usr':cedula,
-                 'alias_usr':alias,
-                 'contrasena_usr': contraseña,
-                 'correo_usr': correo,
-                 'nombres_usr': nombres,
-                 'apellidos_usr': apellidos,
-                 'direccion_usr': direccion,
-                 'telefono_usr': telefono,
-                 'tipoUsr' : '1'
-             })
-                     
-         }) 
-        
-         if(crearUsuario.status != 200) {
-             alertas('alert-denied', 'Lo sentimos el correo ingresado ya existe , Intentelo con otro correo ' )
-         }else{
-             alertas('alert-accepted','El usuario se ha creado correctamente , Puedes hacer login')
-         }
-        }
+                let crearUsuario = await fetch(API,{
+                    method:'POST',
+                    mode:'cors',
+                    credentials:'same-origin',
+                    headers:{
+                        'Content-Type' : 'application/json;'
+                    },
+                    body:JSON.stringify({
+                        'idcedula_usr':cedula,
+                        'alias_usr':alias,
+                        'contrasena_usr': contraseña,
+                        'correo_usr': correo,
+                        'nombres_usr': nombres,
+                        'apellidos_usr': apellidos,
+                        'direccion_usr': direccion,
+                        'telefono_usr': telefono,
+                        'tipoUsr' : '1'
+                    })
+                            
+                }) 
+            
+                if(crearUsuario.status != 200) {
+                    alertas('alert-denied', 'Lo sentimos , A ocurrido un error,  Intentelo mas tarde' )
+                    console.log(`Error de conexion , estatus de la peticción : ${crearUsuario.status}`)
+                }else{
+                    alertas('alert-accepted','El usuario se ha creado correctamente')
+                    let btn_registro = document.getElementById('btn-registro')
+
+                    btn_registro.onclick = setTimeout(function(){
+                        location.href="../cliente/main.html"
+                    },2000)
+                }
+            }
            
     })
 
 }
 
 
+
+// previniendo el evento onsubmit del formulario registro y creando el usuario 
 try {
-    
     const formulario_registro = document.getElementById('singUp-form')
-    
     formulario_registro.onsubmit = (e)=>{
         e.preventDefault()
         
@@ -136,7 +152,7 @@ try {
         let direccion_usr =  document.getElementById('direccion')
         let telefono_usr = document.getElementById('telefono')
        
-        btn_registro.addEventListener('click',crearUnUsuario(alias_usr.value , nombres_usr.value , apellidos_usr.value , idcedula_usr.value , direccion_usr.value , telefono_usr.value , emailInput.value, inputPassword.value))
+        crearUnUsuario(alias_usr.value , nombres_usr.value , apellidos_usr.value , idcedula_usr.value , direccion_usr.value , telefono_usr.value , emailInput.value, inputPassword.value)
                 
         alias_usr.value = ""
         nombres_usr.value = ""
@@ -153,3 +169,39 @@ try {
 
 
 
+// previniendo el evento onsubmit del formulario login  y haciendo login 
+
+try {
+    const formulario_login = document.getElementById('form-login')
+    const btn_login = document.getElementById('login')
+    formulario_login.onsubmit=(e)=>{
+        
+        e.preventDefault()
+        
+        let usuarios = obtenerUsuario(API)
+        usuarios.then(usrs=> {
+            let arrayUsuarios = Array.from(usrs)
+        
+            let usr = arrayUsuarios.find(e=> e.correo_usr == emailInput.value) ?? false
+           
+
+            if(usr.correo_usr == emailInput.value && usr.contrasena_usr == inputPassword.value){
+                alertas('alert-accepted','Bienvenido ' + usr.alias_usr)
+
+                btn_login.onclick = setTimeout(function(){
+                    location.href="../cliente/main.html"
+                },100000)
+                        
+            }else{
+                alertas('alert-denied', 'El email o la contraseña no ha sido encontrado , Por favor revise que la información añadida esté escrita correctamente , Caso contrario realize el formulario de registro')
+            }
+
+
+            emailInput.value= ""
+            inputPassword.value = ""
+        })
+
+    }   
+} catch (error) {
+    console.log(error)
+}
